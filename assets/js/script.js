30f0e24290fbf584c922ec9532306cb527af7b11
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-05 11:52:50 +0000

    add event listener add function to generate two random numbers between 1 and 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
 
+  for (let button of buttons){
+    button.addEventListener("click", function(){
+
+    if (this.getAttribute("data-type")=== "submit"){
+        alert("you clicked submit!");
+    }else{
+         let gametype = this.getAttribute("data-type");  
+         alert(`You clicked ${gametype}`)         
+    }  
+    })
+  }
+
+
+
+})
+
+/**
+ * the main game loop 
+ */
+
+function runGame(){
+let num1 = Math.floor(Math.random() *25)+1; 
+let num2 = Math.floor(Math.random() *25)+1; 
 }
+
+
 
 function checkAnswer(){
 
@@ -31,5 +57,5 @@ function dispalyMultiplyQuestion(){
 }
 
 function displayDivideQuestion(){
-    
+
 }
