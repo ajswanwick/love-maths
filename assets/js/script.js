cd89d1dab3d0e838414606dbf25f58f90aa2a12f
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-05 13:30:17 +0000

    add addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
     if (this.getAttribute("data-type")=== "submit"){
         alert("you clicked submit!");
     }else{
-         let gametype = this.getAttribute("data-type");  
-         alert(`You clicked ${gametype}`)         
+         let gameType = this.getAttribute("data-type");  
+         alert(`You clicked ${gameType}`)    
+         runGame(gameType);     
     }  
     })
   }
@@ -21,9 +22,18 @@ runGame("addition");
  * the main game loop 
  */
 
-function runGame(){
+function runGame(gameType){
 let num1 = Math.floor(Math.random() *25)+1; 
 let num2 = Math.floor(Math.random() *25)+1; 
+  
+ if (gameType === "addition"){
+      displayAdditionQuestion(num1,num2);
+ }else{
+   alert(`Unknown game type: ${gameType}`);
+   throw `unknown game type: ${gameType}. Aborting `;
+
+ }
+
 }
 
 
@@ -44,8 +54,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+"
 }
 
 function dispalySubtractQuestion() {
