1ec10d3eca469034b2a51b1eae8169e2e7b97f11
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-06 11:19:31 +0000

    update division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  document.getElementById('answer-box').addEventListener('keydown', function(event){
+     if (event.key === 'Enter'){
+      checkAnswer()
+
+     }
+  })
+
 runGame("addition");
 
 });
@@ -22,6 +29,10 @@ runGame("addition");
  */
 
 function runGame(gameType){
+
+  document.getElementById('answer-box').value ="";
+  document.getElementById('answer-box').focus();
+
 let num1 = Math.floor(Math.random() *25)+1; 
 let num2 = Math.floor(Math.random() *25)+1; 
   
@@ -130,8 +141,10 @@ function dispalyMultiplyQuestion(operand1, operand2){
 
 
 function displayDivideQuestion(operand1,operand2){
-  document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2
-  document.getElementById(`operand2`).textContent = operand1 > operand2 ? operand2 : operand1
+
+   
+  document.getElementById(`operand1`).textContent = operand1 * operand2 ;
+  document.getElementById(`operand2`).textContent = operand2;
   document.getElementById(`operator`).textContent = "/";
 
 }
