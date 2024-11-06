204ae9201bed379b9822861e1540296d5199d4b8
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-06 11:12:14 +0000

    update event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,13 @@ let num2 = Math.floor(Math.random() *25)+1;
   
  if (gameType === "addition"){
       displayAdditionQuestion(num1,num2);
- }else{
+ }else if (gameType === 'multiply'){
+      dispalyMultiplyQuestion(num1, num2);
+}else if(gameType === 'subtract'){
+  dispalySubtractQuestion(num1,num2)
+}else if (gameType === 'division'){
+displayDivideQuestion(num1, num2)
+}else{
    alert(`Unknown game type: ${gameType}`);
    throw `unknown game type: ${gameType}. Aborting `;
 
@@ -67,11 +73,19 @@ let operator = document.getElementById("operator").innerText;
 
 if (operator === '+'){
   return [operand1 + operand2, "addition"];
+}else if (operator === 'x'){
+  return[operand1 * operand2, "multiply"];
+}else if (operator === '-'){
+  return[operand1 - operand2, 'subtract'];
+}else if (operator === '/'){
+  return[operand1 / operand2, 'division']
 }else{
   alert(`unimplemented operator ${operator}`)
   throw `unimplemented operator ${operator}. Aborting`
 
 }
+
+
 }
 /**
  * gets current score from dom
@@ -96,14 +110,28 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById(`operator`).textContent = "+";
 }
 
-function dispalySubtractQuestion() {
+function dispalySubtractQuestion(operand1, operand2) {
+  document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2
+    document.getElementById(`operand2`).textContent = operand1 >operand1 ? operand2 : operand1
+    document.getElementById(`operator`).textContent = "-";
+
+   
+}
     
+
+
+function dispalyMultiplyQuestion(operand1, operand2){
+  document.getElementById(`operand1`).textContent = operand1;
+  document.getElementById(`operand2`).textContent = operand2;
+  document.getElementById(`operator`).textContent = "x";
 }
 
-function dispalyMultiplyQuestion(){
 
-}
 
-function displayDivideQuestion(){
+
+function displayDivideQuestion(operand1,operand2){
+  document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2
+  document.getElementById(`operand2`).textContent = operand1 > operand2 ? operand2 : operand1
+  document.getElementById(`operator`).textContent = "/";
 
 }
